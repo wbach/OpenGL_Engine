81413f218ab22e6dbabc51e4b1df1a580e3d41ec
Wojciech Bach <wbach.projects@gmail.com>
2017-08-12 12:57:29 +0200

    [ThridPersonCamera] small refactor

--- GameEngine/Camera/ThridPersonCamera.cpp
+++ GameEngine/Camera/ThridPersonCamera.cpp
@@ -4,12 +4,12 @@
 #include "Utils.h"
 
 CThirdPersonCamera::CThirdPersonCamera(CInputManager* input_manager, CTransform& look_at)
-: inputManager(input_manager)
-, lookAtTransform(look_at)
-, isShowCursor(false)
-, offset(0.0f)
-, mousevel(0.2f)
-, captureMouse(true)
+	: inputManager(input_manager)
+	, lookAtTransform(look_at)
+	, isShowCursor(false)
+	, offset(0.0f)
+	, mousevel(0.2f)
+	, captureMouse(true)
 {
 	distanceFromPlayer = 4.0f;
 }
@@ -18,16 +18,27 @@ void CThirdPersonCamera::SetCaptureMouse(bool capture)
 {
 	captureMouse = capture;
 }
+
+void CThirdPersonCamera::LockPitch()
+{
+	if (pitch > 90.0f)
+		pitch = 90.0f;
+	if (pitch < -90.0f)
+		pitch = -90.0f;
+}
+
+void CThirdPersonCamera::LockYaw()
+{
+	if (yaw < 0.0f)
+		yaw += 360.0f;
+	if (yaw > 360.0f)
+		yaw -= 360.0f;	
+}
+
 void CThirdPersonCamera::LockCamera()
 {
-	if(pitch > 90.0f)
-		pitch = 90.0f;
-	if(pitch < -90.0f)
-		pitch = -90.0f;
-	if(yaw < 0.0f)
-		yaw += 360.0f;
-	if(yaw > 360.0f)
-		yaw -= 360.0f;
+	LockPitch();
+	LockYaw();
 }
 
 void CThirdPersonCamera::CalculateInput()
@@ -40,36 +51,41 @@ void CThirdPersonCamera::CalculateInput()
 
 void CThirdPersonCamera::Move()
 {
-	float horizontal_distance = CalculateHorizontalDistance() ;
-	float vertical_distance	 = CalculateVerticalDistance() ;
+	float horizontal_distance = CalculateHorizontalDistance();
+	float vertical_distance = CalculateVerticalDistance();
 	CalculateCameraPosition(horizontal_distance, vertical_distance);
-	this->yaw = 180 - (lookAtTransform.GetRotation().y + angleAroundPlayer) ;
+	CalculateYaw();
 
 	CCamera::Move();
 }
-void CThirdPersonCamera::SetPosition(vec3 position)
+void CThirdPersonCamera::SetPosition(vec3 position_)
 {
-	position = position;
+	position = position_;
 }
 void CThirdPersonCamera::CalculateCameraPosition(float horizontal_distance, float vertical_distance)
 {
-	float theata  = lookAtTransform.GetRotation().y  + angleAroundPlayer;
-	float x_offset = (float) (horizontal_distance * sin(Utils::ToRadians(theata))) ;
-	float z_offset = (float) (horizontal_distance * cos(Utils::ToRadians(theata))) ;
-	position.x  = lookAtTransform.GetPosition().x - x_offset;
-	position.y  = lookAtTransform.GetPosition().y + vertical_distance + 1.8f;
-	position.z  = lookAtTransform.GetPosition().z - z_offset;
-	position	 += offset;
+	float theata = lookAtTransform.GetRotation().y + angleAroundPlayer;
+	float x_offset = (float) (horizontal_distance * sin(Utils::ToRadians(theata)));
+	float z_offset = (float) (horizontal_distance * cos(Utils::ToRadians(theata)));
+	position.x = lookAtTransform.GetPosition().x - x_offset;
+	position.y = lookAtTransform.GetPosition().y + vertical_distance + 1.8f;
+	position.z = lookAtTransform.GetPosition().z - z_offset;
+	position += offset;
 }
 
 float CThirdPersonCamera::CalculateHorizontalDistance()
 {
-	return (float) (distanceFromPlayer * cos(Utils::ToRadians(pitch))) ;
+	return (float) (distanceFromPlayer * cos(Utils::ToRadians(pitch)));
 }
 
 float CThirdPersonCamera::CalculateVerticalDistance()
 {
-	return (float) (distanceFromPlayer * sin(Utils::ToRadians(pitch))) ;
+	return (float) (distanceFromPlayer * sin(Utils::ToRadians(pitch)));
+}
+
+void CThirdPersonCamera::CalculateYaw()
+{
+	this->yaw = 180 - (lookAtTransform.GetRotation().y + angleAroundPlayer);
 }
 
 void CThirdPersonCamera::CalculateZoom(float zoom_lvl)
@@ -84,11 +100,11 @@ vec2 CThirdPersonCamera::CalcualteMouseMove()
 
 void CThirdPersonCamera::CalculatePitch(vec2 d_move)
 {
-		pitch -= d_move.y;
+	pitch -= d_move.y;
 }
 
 void CThirdPersonCamera::CalculateAngleAroundPlayer(vec2 d_move)
 {
-	float angle_change	 = d_move.x;
+	float angle_change = d_move.x;
 	angleAroundPlayer -= angle_change;
 }
--- GameEngine/Camera/ThridPersonCamera.h
+++ GameEngine/Camera/ThridPersonCamera.h
@@ -14,12 +14,14 @@ public:
 	void CalculateZoom(float zoom_lvl) override;
 private:
 	void LockCamera();
-	void SetCaptureMouse(bool capture);	
+	void SetCaptureMouse(bool capture);
+	void LockPitch();
+	void LockYaw();
 	void CalculateCameraPosition(float horizontal_distance, float vertical_distance);
 	void SetPosition(vec3 position);
 	float CalculateHorizontalDistance();
 	float CalculateVerticalDistance();
-
+	void CalculateYaw();
 	vec2 CalcualteMouseMove();
 	void CalculatePitch(vec2 d_move);
 	void CalculateAngleAroundPlayer(vec2 d_move);
