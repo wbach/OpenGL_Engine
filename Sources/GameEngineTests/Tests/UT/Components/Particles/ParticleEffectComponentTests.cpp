fd5cc8449ae32d280ca6d50e50a9073f6ed7a71c
Wojciech Bach <wbach.projects@gmail.com>
2018-04-27 16:21:57 +0200

    [GameEngineTests] Simple particle count test

--- Sources/GameEngineTests/Tests/UT/Components/Particles/ParticleEffectComponentTests.cpp
+++ Sources/GameEngineTests/Tests/UT/Components/Particles/ParticleEffectComponentTests.cpp
@@ -14,19 +14,36 @@ struct ParicleComponentTestSchould : public BaseComponentTestSchould
 	}
 	virtual void SetUp() override
 	{
-
+		sut_.SetParticlesLimit(1000);
+		sut_.SetEmitFunction(std::bind(&ParicleComponentTestSchould::Emit, this, std::placeholders::_1));
 	}
-
+	GameEngine::Particle Emit(const GameEngine::Particle& p)
+	{
+		return EmitParticle(p);
+	}
+	MOCK_METHOD1(EmitParticle, GameEngine::Particle(const GameEngine::Particle&));
 	ParticleEffectComponent sut_;
 };
 
-
-TEST_F(ParicleComponentTestSchould, GetLastNextFrame)
+TEST_F(ParicleComponentTestSchould, EmitParticlesCountTest)
 {
 	time_.deltaTime = 0.1f;
+	sut_.SetParticlesPerSec(100);
 	sut_.ReqisterFunctions();
 	vec3 camPosition(0);
 	EXPECT_CALL(*camera_, GetPosition()).WillRepeatedly(ReturnRef(camPosition));
+	EXPECT_CALL(*this, EmitParticle(_)).Times(10);
 	componentController_.Update();
-	EXPECT_TRUE(true);
 }
+
+TEST_F(ParicleComponentTestSchould, EmitParticlesCountTest)
+{
+	sut_.SetParticlesPerSec(0);
+	sut_.ReqisterFunctions();
+	vec3 camPosition(0);
+	EXPECT_CALL(*camera_, GetPosition()).WillRepeatedly(ReturnRef(camPosition));
+	EXPECT_CALL(*this, EmitParticle(_)).Times(0);
+
+	componentController_.Update();
+}
+
