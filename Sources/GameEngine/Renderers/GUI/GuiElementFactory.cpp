3a6cf9d5bcbe8288edc6d43e485c61e050b29071
Wojciech Bach <wbach.projects@gmail.com>
2019-05-10 14:23:14 +0200

    GuiElementFactory created first objects

--- Sources/GameEngine/Renderers/GUI/GuiElementFactory.cpp
+++ Sources/GameEngine/Renderers/GUI/GuiElementFactory.cpp
@@ -1,1 +1,60 @@
 #include "GuiElementFactory.h"
+#include "GameEngine/Renderers/RenderersManager.h"
+#include "GameEngine/Resources/ResourceManager.h"
+#include "GuiManager.h"
+#include "Text/GuiTextElement.h"
+#include "Text/GuiTextFactory.h"
+#include "Texutre/GuiTextureElement.h"
+#include "Window/GuiWindow.h"
+
+namespace GameEngine
+{
+GuiElementFactory::GuiElementFactory(GuiElementFactory::EntryParameters &entryParameters)
+    : guiTextFactory_(entryParameters.guiTextFactory_)
+    , guiManager_(entryParameters.guiManager_)
+    , inputManager_(entryParameters.inputManager_)
+    , resourceManager_(entryParameters.resourceManager_)
+    , subscribeForRender_(entryParameters.subscribeForRender_)
+    , windowSize_(entryParameters.windowSize_)
+{
+}
+
+GuiElement *GuiElementFactory::CreateGuiText(const std::string &label, const std::string &font, const std::string &str, uint32 size, uint32 outline)
+{
+    auto text   = guiTextFactory_.Create(font, str, size, outline);
+    auto result = text.get();
+    guiManager_.Add(label, std::move(text));
+    return result;
+}
+
+GuiElement *GuiElementFactory::CreateGuiTexture(const std::string &label, const std::string &filename)
+{
+    auto guiTexture = MakeGuiTexture(filename);
+    auto result     = guiTexture.get();
+    guiManager_.Add(label, std::move(guiTexture));
+    return result;
+}
+
+GuiElement *GuiElementFactory::CreateGuiWindow(const std::string &label, const Rect &rect, const std::string &background)
+{
+    std::cout << __FUNCTION__ << std::endl;
+    auto guiWindow = std::make_unique<GuiWindowElement>(windowSize_, inputManager_);
+    guiWindow->SetRect(rect);
+
+    auto result               = guiWindow.get();
+    auto backgroundGuiTexture = MakeGuiTexture(background);
+    backgroundGuiTexture->SetScale(guiWindow->GetScale());
+    subscribeForRender_(*backgroundGuiTexture);
+    guiWindow->AddChild(std::move(backgroundGuiTexture));
+    guiManager_.Add(label, std::move(guiWindow));
+
+    return result;
+}
+
+std::unique_ptr<GuiElement> GuiElementFactory::MakeGuiTexture(const std::string &filename)
+{
+    auto texture = resourceManager_.GetTextureLaoder().LoadTexture(filename);
+    return std::make_unique<GuiTextureElement>(windowSize_, *texture);
+}
+
+}  // namespace GameEngine
--- Sources/GameEngine/Renderers/GUI/GuiElementFactory.h
+++ Sources/GameEngine/Renderers/GUI/GuiElementFactory.h
@@ -1,9 +1,45 @@
 #pragma once
+#include <functional>
+#include <memory>
+#include "GuiElement.h"
 
 namespace GameEngine
 {
+namespace Input
+{
+class InputManager;
+} // namespace Input
+
+class GuiManager;
+class GuiTextFactory;
+class ResourceManager;
+
 class GuiElementFactory
 {
 public:
+    struct EntryParameters
+    {
+        GuiTextFactory& guiTextFactory_;
+        GuiManager& guiManager_;
+        Input::InputManager& inputManager_;
+        ResourceManager& resourceManager_;
+        std::function<void(GuiElement&)> subscribeForRender_;
+        const vec2ui& windowSize_;
+    };
+    GuiElementFactory(EntryParameters& entryParameters);
+    GuiElement* CreateGuiText(const std::string&, const std::string&, const std::string&, uint32, uint32);
+    GuiElement* CreateGuiTexture(const std::string&, const std::string&);
+    GuiElement* CreateGuiWindow(const std::string&, const Rect&, const std::string&);
+
+private:
+    std::unique_ptr<GuiElement> MakeGuiTexture(const std::string&);
+
+private:
+    GuiTextFactory& guiTextFactory_;
+    GuiManager& guiManager_;
+    Input::InputManager& inputManager_;
+    ResourceManager& resourceManager_;
+    std::function<void(GuiElement&)> subscribeForRender_;
+    const vec2ui& windowSize_;
 };
 }  // namespace GameEngine
