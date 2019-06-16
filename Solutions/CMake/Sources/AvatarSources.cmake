8702b33cf348112b0aeb47c1ae2954d2640a51c4
Wojciech Bach <wbach.projects@gmail.com>
2019-06-16 12:17:17 +0200

    Add mising include

--- Solutions/CMake/Sources/AvatarSources.cmake
+++ Solutions/CMake/Sources/AvatarSources.cmake
@@ -1,8 +1,8 @@
 set(AvatarSources
-../../Sources/Avatar/main.cpp
-../../Sources/Avatar/PlayerInputController.cpp
-../../Sources/Avatar/Scene/SceneFactory.cpp
-../../Sources/Avatar/Scene/Scenes/MainMenu.cpp
-../../Sources/Avatar/Scene/Scenes/SouthPool.cpp
 ../../Sources/Avatar/StartGame.cpp
+../../Sources/Avatar/Scene/SceneFactory.cpp
+../../Sources/Avatar/Scene/Scenes/SouthPool.cpp
+../../Sources/Avatar/Scene/Scenes/MainMenu.cpp
+../../Sources/Avatar/PlayerInputController.cpp
+../../Sources/Avatar/main.cpp
 )
