c66be5f3da1eff67ae8fbe46c96028c395f06514
Wojciech Bach <wbach.projects@gmail.com>
2019-08-09 11:23:28 +0200

    Vertical layout mouse scroll

--- Sources/GameEngine/Renderers/GUI/Layout/VerticalLayout.cpp
+++ Sources/GameEngine/Renderers/GUI/Layout/VerticalLayout.cpp
@@ -4,16 +4,39 @@ namespace GameEngine
 {
 GuiElementTypes VerticalLayout::type = GuiElementTypes::VerticalLayout;
 
-VerticalLayout::VerticalLayout(const vec2ui &windowSize, Input::InputManager &inputManager,
-                               std::function<void(uint32)> unsubscribe)
+VerticalLayout::VerticalLayout(const vec2ui &windowSize, Input::InputManager &inputManager, std::function<void(uint32)> unsubscribe)
     : Layout(type, windowSize, unsubscribe)
     , inputManager_(inputManager)
     , algin_(Algin::CENTER)
+    , viewPosition_(0.f)
+    , totalYScale_(0.f)
 {
+    mouseWheelDownSub_ = inputManager_.SubscribeOnKeyDown(KeyCodes::MOUSE_WHEEL, [this]() {
+        if (IsShow() and viewPosition_ < 0.f)
+        {
+            viewPosition_ += 0.01f;
+            OnChange();
+        }
+    });
+
+    mouseWheelUpSub_ = inputManager_.SubscribeOnKeyUp(KeyCodes::MOUSE_WHEEL, [this]() {
+        if (IsShow() and viewPosition_ > -totalYScale_)
+        {
+            viewPosition_ -= 0.01f;
+            OnChange();
+        }
+    });
+}
+
+VerticalLayout::~VerticalLayout()
+{
+    inputManager_.UnsubscribeOnKeyUp(KeyCodes::MOUSE_WHEEL, mouseWheelUpSub_);
+    inputManager_.UnsubscribeOnKeyDown(KeyCodes::MOUSE_WHEEL, mouseWheelDownSub_);
 }
 
 LayoutElementWrapper &VerticalLayout::AddChild(GuiElement *element)
 {
+    element->SetZPosition(zPosition_);
     children_.emplace_back(*element, [this]() { OnChange(); });
     elements_.push_back(element);
     OnChange();
@@ -32,29 +55,29 @@ void VerticalLayout::OnChange()
         return;
 
     vec2 newPosition = position_;
-
-    newPosition.x = CalculateXPosition(*elements_[0]);
-    newPosition.y += scale_.y - elements_[0]->GetScale().y;
-
+    newPosition.x    = CalculateXPosition(*elements_[0]);
+    newPosition.y += scale_.y - elements_[0]->GetScale().y - viewPosition_;
     elements_[0]->SetPostion(newPosition);
 
     for (std::size_t i = 1; i < elements_.size(); ++i)
     {
-        const auto &oldPosition = elements_[i]->GetPosition();
+        const auto &oldPosition     = elements_[i]->GetPosition();
         const auto &parentPositionY = elements_[i - 1]->GetPosition().y;
         const auto &parentScaleY    = elements_[i - 1]->GetScale().y;
 
         newPosition.x = CalculateXPosition(*elements_[i]);
-        newPosition.y = parentPositionY - 2.f * parentScaleY;
+        newPosition.y = parentPositionY - (2.f * parentScaleY);
 
         if (oldPosition != newPosition)
         {
             elements_[i]->SetPostion(newPosition);
         }
     }
+
+    UpdateVisibility();
 }
 
-float VerticalLayout::CalculateXPosition(const GuiElement & element)
+float VerticalLayout::CalculateXPosition(const GuiElement &element)
 {
     float result = position_.x;
 
@@ -68,6 +91,25 @@ float VerticalLayout::CalculateXPosition(const GuiElement & element)
     }
 
     return result;
+}
+
+void VerticalLayout::UpdateVisibility()
+{
+    totalYScale_ = 0.f;
+
+    for (auto &element : elements_)
+    {
+        if (element->GetPosition().y - element->GetScale().y < position_.y - scale_.y or element->GetPosition().y + element->GetScale().y > position_.y + scale_.y)
+        {
+            element->Hide();
+        }
+        else
+        {
+            element->Show();
+        }
+
+        totalYScale_ += element->GetScale().y;
+    }
 }
 
 }  // namespace GameEngine
--- Sources/GameEngine/Renderers/GUI/Layout/VerticalLayout.h
+++ Sources/GameEngine/Renderers/GUI/Layout/VerticalLayout.h
@@ -14,16 +14,22 @@ public:
     };
 
     VerticalLayout(const vec2ui& windowSize, Input::InputManager& inputManager, std::function<void(uint32)> unsubscribe);
+    ~VerticalLayout() override;
     virtual LayoutElementWrapper& AddChild(GuiElement*) override;
     void SetAlgin(Algin algin);
 
 private:
     void OnChange();
     float CalculateXPosition(const GuiElement&);
+    void UpdateVisibility();
 
 private:
     Input::InputManager& inputManager_;
     Algin algin_;
+    float viewPosition_;
+    uint32 mouseWheelUpSub_;
+    uint32 mouseWheelDownSub_;
+    float totalYScale_;
 
 public:
     static GuiElementTypes type;
