fa387930ad057ce6b63efd673ade89c4c91a7e6c
Wojciech Bach <wbach.projects@gmail.com>
2019-09-11 08:15:17 +0200

    Install sublime

--- ProjectUtils/InstallStartupPackages.sh
+++ ProjectUtils/InstallStartupPackages.sh
@@ -18,6 +18,7 @@ sudo apt-get install -y linux-tools-common
 sudo apt-get install -y libboost-all-dev
 sudo apt-get install -y font-manager
 sudo apt-get install -y lm-sensors
+sudo apt-get install -y clang-format
 
 sudo apt-get install -y python-pip
 sudo apt-get install -y python-opencv
@@ -39,6 +40,12 @@ sudo apt-get install -y libassimp-dev
 sudo apt-get install -y libfreeimage3 libfreeimage-dev
 sudo apt-get install -y libfreetype6-dev
 sudo apt-get install -y libsdl2-ttf-dev
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y sublime-text
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --update-rc
