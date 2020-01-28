336e5b8eaf795e75b43d1170a800838f2e72b7de
Wojciech Bach <wbach.projects@gmail.com>
2020-01-28 14:47:33 +0100

    wip

--- Sources/Editor/Editor3.py
+++ Sources/Editor/Editor3.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter.ttk import *
+
 import socket
 import json
 from tkinter import filedialog
@@ -63,22 +65,55 @@ def connect():
 
 def OpenFile():
     filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("scene files","*.xml"),("all files","*.*")))
-    SendOpenFileCommand("openFile " + filename);
+    if filename:
+        SendOpenFileCommand("openFile " + filename);
 
 def Close():
     s.close()
 
 window = Tk()
 window.title("Editor")
-window.geometry('350x200')
+window.geometry('405x800')
 
-btn = Button(window, text="Connect", command=connect) 
-btn.grid(column=1, row=0)
+# btn = Button(window, text="Connect", command=connect) 
+# btn.grid(column=0, row=0, sticky=(N, S, E, W))
 
-btn = Button(window, text="OpenFile", command=OpenFile) 
-btn.grid(column=1, row=1)
+tree=Treeview(window)
+tree.grid(columnspan=3, row=1)
+tree["columns"]=("one","two","three")
+tree.column("#0", width=100, minwidth=100)
+tree.column("one", width=100, minwidth=100)
+tree.column("two", width=100, minwidth=100)
+tree.column("three", width=100, minwidth=100)
 
-btn = Button(window, text="Close", command=Close) 
-btn.grid(column=1, row=2)
+tree.heading("#0",text="Name")
+tree.heading("one", text="Date modified")
+tree.heading("two", text="Type")
+tree.heading("three", text="Size")
+
+# Level 1
+folder1=tree.insert("", 1, None, text="Folder 1", values=("23-Jun-17 11:05","File folder",""))
+# tree.insert("", 2, "", text="text_file.txt", values=("23-Jun-17 11:25","TXT file","1 KB"))
+# # Level 2
+tree.insert(folder1, "end", None, text="photo1.png", values=("23-Jun-17 11:28","PNG file","2.6 KB"))
+
+# tree.insert(folder1, "end", "", text="photo2.png", values=("23-Jun-17 11:29","PNG file","3.2 KB"))
+# tree.insert(folder1, "end", "", text="photo3.png", values=("23-Jun-17 11:30","PNG file","3.1 KB"))
+
+def donothing():
+    return
+
+menubar = Menu(window)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Connect", command=connect)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=OpenFile)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_command(label="Save as...", command=donothing)
+filemenu.add_command(label="Close", command=Close)
+menubar.add_cascade(label="File", menu=filemenu)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=window.quit)
+window.config(menu=menubar)
 
 window.mainloop()
