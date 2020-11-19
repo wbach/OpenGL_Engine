e7075d6d46d975e250c3997a182463e89ffbcfa4
Wojciech Bach <wbach.projects@gmail.com>
2020-11-19 17:52:23 +0100

    [GameEngine] Cleanup

--- Sources/GameEngine/Renderers/Objects/Terrain/Mesh/ConcreteTerrainMeshRenderer.h
+++ Sources/GameEngine/Renderers/Objects/Terrain/Mesh/ConcreteTerrainMeshRenderer.h
@@ -40,13 +40,7 @@ public:
     void render() override;
 
 private:
-    void bindTexture(Texture*, uint32 id) const;
     void bindShadowMap(uint32 id, uint32 nr) const;
-    void bindTextures(const std::vector<std::pair<TerrainTextureType, Texture*>>&) const;
-    void renderSubscribers() const;
-    void renderSubscriber(const Subscriber&) const;
-    void renderMesh(const Mesh&, const GraphicsApi::ID&) const;
-    void partialRendering(const Model&, const Components::TerrainMeshRendererComponent&) const;
 
 private:
     RendererContext& context_;
