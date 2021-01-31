074a9001bb119e0229c33b19c642dc666fe122f6
Wojciech Bach <wbach.projects@gmail.com>
2021-02-01 00:01:16 +0100

    [GameEngine] loader addlogs

--- Sources/GameEngine/Resources/Models/WBLoader/LoaderManager.cpp
+++ Sources/GameEngine/Resources/Models/WBLoader/LoaderManager.cpp
@@ -1,16 +1,18 @@
 #include "LoaderManager.h"
-#include "Collada/Collada.h"
-#include "Fbx/FbxLoader.h"
-#include "Terrain/TerrainMeshLoader.h"
-#include "WaveFront/WaveFrontObj.h"
-#include "Assimp/AssimpLoader.h"
-#include "BEngine/BEngineLoader.h"
-#include "GameEngine/Engine/Configuration.h"
 
 #include <Logger/Log.h>
+
 #include <algorithm>
 #include <sstream>
+
+#include "Assimp/AssimpLoader.h"
+#include "BEngine/BEngineLoader.h"
+#include "Collada/Collada.h"
+#include "Fbx/FbxLoader.h"
+#include "GameEngine/Engine/Configuration.h"
 #include "ParseUtils.h"
+#include "Terrain/TerrainMeshLoader.h"
+#include "WaveFront/WaveFrontObj.h"
 
 namespace GameEngine
 {
@@ -18,10 +20,10 @@ LoaderManager::LoaderManager(ITextureLoader& textureloader)
     : textureloader(textureloader)
 {
     loaders_.emplace_back(new WBLoader::BEngineLoader(textureloader));
-   // loaders_.emplace_back(new WBLoader::WaveFrontObjLoader(textureloader));
-   // loaders_.emplace_back(new WBLoader::ColladaDae(textureloader));
+    // loaders_.emplace_back(new WBLoader::WaveFrontObjLoader(textureloader));
+    // loaders_.emplace_back(new WBLoader::ColladaDae(textureloader));
     loaders_.emplace_back(new WBLoader::TerrainMeshLoader(textureloader));
-    //loaders_.emplace_back(new WBLoader::FbxLoader(textureloader));
+    // loaders_.emplace_back(new WBLoader::FbxLoader(textureloader));
     loaders_.emplace_back(new WBLoader::AssimpLoader(textureloader));
 }
 
@@ -37,7 +39,26 @@ std::unique_ptr<Model> LoaderManager::Load(const File& file)
 
     loaderPtr->Parse(file);
     auto result = loaderPtr->Create();
-    result->SetFile(file);
+    if (result)
+    {
+        result->SetFile(file);
+
+        DEBUG_LOG("File : " + file.GetDataRelativeDir());
+        DEBUG_LOG("Meshes count : " + std::to_string(result->GetMeshes().size()));
+
+        auto trianglesCount = 0u;
+        for (const auto& mesh : result->GetMeshes())
+        {
+            trianglesCount += (not mesh.GetCMeshDataRef().indices_.empty())
+                                  ? (mesh.GetCMeshDataRef().indices_.size() / 3.f)
+                                  : (mesh.GetCMeshDataRef().positions_.size() / 3.f / 3.f);
+        }
+        DEBUG_LOG("Total triangles count : " + std::to_string(trianglesCount));
+    }
+    else
+    {
+        ERROR_LOG("File  creation error: " + file.GetDataRelativeDir());
+    }
     return result;
 }
 
