3bef1800bcedc9c6aba6f366d333f2aadef88d1a
Wojciech Bach <wbach.projects@gmail.com>
2021-02-25 23:17:48 +0100

    x86 working, x64 not

--- Sources/Editor/main.cpp
+++ Sources/Editor/main.cpp
@@ -2,96 +2,184 @@
 #include <GameEngine/Engine/Engine.h>
 #include <Logger/Log.h>
 #include <OpenGLApi/OpenGLApi.h>
-
-#include "Editor/Context.h"
-#include "GameEngine/Engine/Engine.h"
-#include "GameEngine/Physics/Bach/BachPhysicsAdapter.h"
-#include "GameEngine/Physics/Bullet/BulletAdapter.h"
-#include "Scene/SceneFactory.h"
+// wxWidgets "Hello world" Program
+// For compilers that support precompilation, includes "wx/wx.h".
 
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
 
-const std::string configFile = "./Conf.xml";
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/dataview.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/string.h>
+#include <wx/sizer.h>
+#include <wx/panel.h>
+#include <wx/aui/auibook.h>
+#include <wx/dirctrl.h>
+#include <wx/frame.h>
 
-using namespace GameEngine;
-using namespace GameEngine::Physics;
+class MyFrame1 : public wxFrame
+{
+private:
+
+protected:
+	wxDataViewTreeCtrl* m_dataViewTreeCtrl4;
+	wxPanel* m_panel3;
+	wxAuiNotebook* m_auinotebook1;
+	wxGenericDirCtrl* m_genericDirCtrl2;
+	wxPanel* m_panel2;
+
+public:
+
+	MyFrame1(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(1567, 683), long style = wxDEFAULT_FRAME_STYLE | wxTAB_TRAVERSAL);
+
+	~MyFrame1();
+
+};
+
+MyFrame1::MyFrame1(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style) : wxFrame(parent, id, title, pos, size, style)
+{
+	this->SetSizeHints(wxDefaultSize, wxDefaultSize);
+	this->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWFRAME));
+
+	wxBoxSizer* bSizer7;
+	bSizer7 = new wxBoxSizer(wxVERTICAL);
+
+	wxBoxSizer* bSizer8;
+	bSizer8 = new wxBoxSizer(wxHORIZONTAL);
+
+	wxBoxSizer* bSizer10;
+	bSizer10 = new wxBoxSizer(wxVERTICAL);
+
+	m_dataViewTreeCtrl4 = new wxDataViewTreeCtrl(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0);
+	bSizer10->Add(m_dataViewTreeCtrl4, 0, wxALL, 5);
+
+
+	bSizer8->Add(bSizer10, 1, wxEXPAND, 5);
+
+	wxBoxSizer* bSizer11;
+	bSizer11 = new wxBoxSizer(wxVERTICAL);
+
+	m_panel3 = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+	bSizer11->Add(m_panel3, 1, wxEXPAND | wxALL, 5);
+
+
+	bSizer8->Add(bSizer11, 1, wxEXPAND, 5);
+
+	wxBoxSizer* bSizer12;
+	bSizer12 = new wxBoxSizer(wxVERTICAL);
+
+	m_auinotebook1 = new wxAuiNotebook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxAUI_NB_DEFAULT_STYLE);
+
+	bSizer12->Add(m_auinotebook1, 1, wxEXPAND | wxALL, 5);
+
+
+	bSizer8->Add(bSizer12, 1, wxEXPAND, 5);
+
+
+	bSizer7->Add(bSizer8, 1, wxEXPAND, 5);
+
+	wxBoxSizer* bSizer9;
+	bSizer9 = new wxBoxSizer(wxHORIZONTAL);
+
+	wxBoxSizer* bSizer13;
+	bSizer13 = new wxBoxSizer(wxVERTICAL);
+
+	m_genericDirCtrl2 = new wxGenericDirCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDIRCTRL_3D_INTERNAL | wxSUNKEN_BORDER, wxEmptyString, 0);
+
+	m_genericDirCtrl2->ShowHidden(false);
+	bSizer13->Add(m_genericDirCtrl2, 1, wxEXPAND | wxALL, 5);
+
+
+	bSizer9->Add(bSizer13, 1, wxEXPAND, 5);
+
+	wxBoxSizer* bSizer14;
+	bSizer14 = new wxBoxSizer(wxVERTICAL);
+
+	m_panel2 = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+	bSizer14->Add(m_panel2, 1, wxEXPAND | wxALL, 5);
+
+
+	bSizer9->Add(bSizer14, 1, wxEXPAND, 5);
+
+
+	bSizer7->Add(bSizer9, 1, wxEXPAND, 5);
+
+
+	this->SetSizer(bSizer7);
+	this->Layout();
+
+	this->Centre(wxBOTH);
+}
+
+MyFrame1::~MyFrame1()
+{
+}
 
 class MyApp : public wxApp
 {
 public:
-    virtual bool OnInit();
+	virtual bool OnInit();
 };
-
 class MyFrame : public wxFrame
 {
 public:
-    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
-
+	MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
 private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-    wxDECLARE_EVENT_TABLE();
+	void OnHello(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
+	void OnAbout(wxCommandEvent& event);
+	wxDECLARE_EVENT_TABLE();
 };
-
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
-}
-
-void MyFrame::OnExit(wxCommandEvent& event)
-{
-    Close(true);
-}
-
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets' Hello world sample",
-        "About Hello World", wxOK | wxICON_INFORMATION);
-}
 enum
 {
-    ID_Hello = 1
+	ID_Hello = 1
 };
-
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
 EVT_MENU(ID_Hello, MyFrame::OnHello)
 EVT_MENU(wxID_EXIT, MyFrame::OnExit)
 EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
 wxEND_EVENT_TABLE()
-
 wxIMPLEMENT_APP(MyApp);
-
 bool MyApp::OnInit()
 {
-    if (!wxApp::OnInit())
-        return false;
-
-    MyFrame* frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
-
-    frame->Show(true);
-    return true;
+	auto* frame = new MyFrame1(nullptr);
+	frame->Show(true);
+	return true;
 }
-
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-    : wxFrame(NULL, wxID_ANY, title, pos, size)
+	: wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-    wxMenu* menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-        "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-    wxMenu* menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
-    wxMenuBar* menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
-
-    SetMenuBar(menuBar);
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
+	wxMenu* menuFile = new wxMenu;
+	menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+		"Help string shown in status bar for this menu item");
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_EXIT);
+	wxMenu* menuHelp = new wxMenu;
+	menuHelp->Append(wxID_ABOUT);
+	wxMenuBar* menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, "&File");
+	menuBar->Append(menuHelp, "&Help");
+	SetMenuBar(menuBar);
+	CreateStatusBar();
+	SetStatusText("Welcome to wxWidgets!");
 }
-
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+	Close(true);
+}
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+	wxMessageBox("This is a wxWidgets' Hello world sample",
+		"About Hello World", wxOK | wxICON_INFORMATION);
+}
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+	wxLogMessage("Hello world from wxWidgets!");
+}
