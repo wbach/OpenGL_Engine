9a4bfb9936e79de159816971f7f10929c87dc987
wbach <wbach.projects@gmail.com>
2024-02-01 19:11:06 +0100

    Death state
    armed / disarmed anim base on prev state

--- Sources/GameEngine/Components/Controllers/CharacterController/States/DeathState.cpp
+++ Sources/GameEngine/Components/Controllers/CharacterController/States/DeathState.cpp
@@ -9,17 +9,198 @@ DeathState::DeathState(FsmContext &context)
 {
 }
 
-void DeathState::onEnter()
-{
-    if (not context_.animClipNames.disarmed.death.empty())
-    {
-        context_.animator.ChangeAnimation(context_.animClipNames.disarmed.death, Animator::AnimationChangeType::smooth,
-                                          PlayDirection::forward, std::nullopt);
-    }
-}
-
 void DeathState::update(float)
 {
+}
+
+void DeathState::onEnter(DisarmedIdleState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.disarmed.death);
+}
+
+void DeathState::onEnter(DisarmedRunState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.disarmed.death);
+}
+
+void DeathState::onEnter(DisarmedRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.disarmed.death);
+}
+
+void DeathState::onEnter(DisarmedRunAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.disarmed.death);
+}
+
+void DeathState::onEnter(DisarmedWalkState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.disarmed.death);
+}
+
+void DeathState::onEnter(DisarmedWalkAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.disarmed.death);
+}
+
+void DeathState::onEnter(DisarmedSprintState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.disarmed.death);
+}
+
+void DeathState::onEnter(DisarmedSprintAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.disarmed.death);
+}
+
+void DeathState::onEnter(ArmedIdleState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(ArmedRunState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(ArmedRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(ArmedRunAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(ArmedWalkState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(ArmedWalkAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(ArmedSprintState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(ArmedSprintAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(JumpState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(MoveJumpState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(AimState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(AimRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(AimRunState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(AimRunAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(AimWalkState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(AimWalkAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(RecoilState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(RecoilRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(RecoilRunState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(RecoilRunAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(RecoilWalkState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(RecoilWalkAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(DrawArrowState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(DrawArrowRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(DrawArrowRunState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(DrawArrowRunAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(DrawArrowWalkState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::onEnter(DrawArrowWalkAndRotateState &, const DeathEvent &)
+{
+    setAnim(context_.animClipNames.armed.death);
+}
+
+void DeathState::setAnim(const std::string &animName)
+{
+    context_.multiAnimations = false;
+    if (not animName.empty())
+    {
+        context_.animator.ChangeAnimation(animName, Animator::AnimationChangeType::smooth, PlayDirection::forward,
+                                          std::nullopt);
+    }
 }
 
 }  // namespace Components
--- Sources/GameEngine/Components/Controllers/CharacterController/States/DeathState.h
+++ Sources/GameEngine/Components/Controllers/CharacterController/States/DeathState.h
@@ -9,12 +9,88 @@ namespace GameEngine
 {
 namespace Components
 {
+class DisarmedIdleState;
+class DisarmedRunState;
+class DisarmedRotateState;
+class DisarmedRunAndRotateState;
+class DisarmedWalkState;
+class DisarmedWalkAndRotateState;
+class DisarmedSprintState;
+class DisarmedSprintAndRotateState;
+class ArmedIdleState;
+class ArmedRunState;
+class ArmedRotateState;
+class ArmedRunAndRotateState;
+class ArmedWalkState;
+class ArmedWalkAndRotateState;
+class ArmedSprintState;
+class ArmedSprintAndRotateState;
+class JumpState;
+class MoveJumpState;
+class AimState;
+class AimRotateState;
+class AimRunState;
+class AimRunAndRotateState;
+class AimWalkState;
+class AimWalkAndRotateState;
+class RecoilState;
+class RecoilRotateState;
+class RecoilRunState;
+class RecoilRunAndRotateState;
+class RecoilWalkState;
+class RecoilWalkAndRotateState;
+class DrawArrowState;
+class DrawArrowRotateState;
+class DrawArrowRunState;
+class DrawArrowRunAndRotateState;
+class DrawArrowWalkState;
+class DrawArrowWalkAndRotateState;
+
 class DeathState : public Utils::StateMachine::Will<Utils::StateMachine::ByDefault<Utils::StateMachine::Nothing>>
 {
 public:
     DeathState(FsmContext&);
-    void onEnter();
     void update(float);
+
+    void onEnter(DisarmedIdleState&, const DeathEvent&);
+    void onEnter(DisarmedRunState&, const DeathEvent&);
+    void onEnter(DisarmedRotateState&, const DeathEvent&);
+    void onEnter(DisarmedRunAndRotateState&, const DeathEvent&);
+    void onEnter(DisarmedWalkState&, const DeathEvent&);
+    void onEnter(DisarmedWalkAndRotateState&, const DeathEvent&);
+    void onEnter(DisarmedSprintState&, const DeathEvent&);
+    void onEnter(DisarmedSprintAndRotateState&, const DeathEvent&);
+    void onEnter(ArmedIdleState&, const DeathEvent&);
+    void onEnter(ArmedRunState&, const DeathEvent&);
+    void onEnter(ArmedRotateState&, const DeathEvent&);
+    void onEnter(ArmedRunAndRotateState&, const DeathEvent&);
+    void onEnter(ArmedWalkState&, const DeathEvent&);
+    void onEnter(ArmedWalkAndRotateState&, const DeathEvent&);
+    void onEnter(ArmedSprintState&, const DeathEvent&);
+    void onEnter(ArmedSprintAndRotateState&, const DeathEvent&);
+    void onEnter(JumpState&, const DeathEvent&);
+    void onEnter(MoveJumpState&, const DeathEvent&);
+    void onEnter(AimState&, const DeathEvent&);
+    void onEnter(AimRotateState&, const DeathEvent&);
+    void onEnter(AimRunState&, const DeathEvent&);
+    void onEnter(AimRunAndRotateState&, const DeathEvent&);
+    void onEnter(AimWalkState&, const DeathEvent&);
+    void onEnter(AimWalkAndRotateState&, const DeathEvent&);
+    void onEnter(RecoilState&, const DeathEvent&);
+    void onEnter(RecoilRotateState&, const DeathEvent&);
+    void onEnter(RecoilRunState&, const DeathEvent&);
+    void onEnter(RecoilRunAndRotateState&, const DeathEvent&);
+    void onEnter(RecoilWalkState&, const DeathEvent&);
+    void onEnter(RecoilWalkAndRotateState&, const DeathEvent&);
+    void onEnter(DrawArrowState&, const DeathEvent&);
+    void onEnter(DrawArrowRotateState&, const DeathEvent&);
+    void onEnter(DrawArrowRunState&, const DeathEvent&);
+    void onEnter(DrawArrowRunAndRotateState&, const DeathEvent&);
+    void onEnter(DrawArrowWalkState&, const DeathEvent&);
+    void onEnter(DrawArrowWalkAndRotateState&, const DeathEvent&);
+
+private:
+    void setAnim(const std::string&);
 
 private:
     FsmContext& context_;
