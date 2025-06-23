3d6077c45b7902becbc8b3deea9903e8e03da627
wbach <wbach.projects@gmail.com>
2025-06-23 22:02:37 +0200

    Update blender_exporter.py

--- ProjectUtils/Python/blender_exporter.py
+++ ProjectUtils/Python/blender_exporter.py
@@ -11,20 +11,6 @@ class Vector:
         self.y = y
         self.z = z
 
-
-# id = 1
-# for mesh in bpy.data.meshes:
-#     objectNode = ET.SubElement(gameObjectsNode, "gameObject" )
-#     objectNode.attrib["id"] = str(id)
-#     objectNode.attrib["name"] = mesh.name_full
-
-#     localTransformNode = ET.SubElement(objectNode, "localTransform")
-#     positionNode = ET.SubElement(localTransformNode, "position")
-#  #   objectNode.attrib["x"] = str(mesh.)
-#     rotationNode = ET.SubElement(localTransformNode, "rotation")
-#     scaleNode = ET.SubElement(localTransformNode, "scale")
-#     id = id + 1
-
 def ToDegrees(a):
     return a * 180.0 / 3.14159265358979323846264338327950288
 
@@ -95,22 +81,6 @@ def createRigidbodyNode(componentsNode):
     ET.SubElement(rigidbodyNode, "collisionShape")
 
 def createCollisionComponents(componentsNode, colliderFile):
-    # <component type="MeshShape">
-    #     <positionOffset z="0.000000" y="0.000000" x="0.000000"/>
-    #     <size>1.000000</size>
-    #     <autoOptimize>false</autoOptimize>
-    #     <modelFileName>
-    #         <fileName/>
-    #         <modelNormalization>true</modelNormalization>
-    #         <meshOptimize>true</meshOptimize>
-    #     </modelFileName>
-    # </component>
-    # <component type="Rigidbody">
-    #     <mass>0.000000</mass>
-    #     <isStatic>false</isStatic>
-    #     <noConctactResponse>false</noConctactResponse>
-    #     <collisionShape/>
-    # </component>
     createMeshShapeNode(componentsNode, colliderFile)
     createRigidbodyNode(componentsNode)
 
@@ -131,36 +101,13 @@ def createLocalTransformNode(parentNode, position, rotation, scale):
     scaleNode.attrib["y"] = str(scale.y)
     scaleNode.attrib["z"] = str(scale.z)
 
-# // @param in 3d position vector in Right handed Coordinate System
-# // @param out 3d position vector in Unity Coordinate System
-# void RHSToUnity(Vector3 in, ref Vector3 out)
-# {
-#   out[0] = in[1];
-#   out[1] = in[2];
-#   out[2] = -in[0];
-# }
-
 def convertAxis(v):
     out = copy.deepcopy(v)
-    # out[0] = v[1]
-    # out[1] = v[2]
-    # out[2] = -v[0]
-    # out[0] = -v[2]
-    # out[1] = v[0]
-    # out[2] = v[1]
     out[0] = v[0]
     out[1] = v[2]
     out[2] = v[1]
     return out
 
-# // @param in 3d position vector in Unity Coordinate System
-# // @param out 3d position vector in Right handed Coordinate System
-# void UnityToRHS(Vector3 in, ref Vector3 out)
-# {
-#   out[0] = -in[2];
-#   out[1] = in[0];
-#   out[2] = in[1];
-# }
 
 prefabNode = ET.Element("prefab")
 prefabNode.attrib["name"] = bpy.path.basename(bpy.context.blend_data.filepath)
@@ -223,40 +170,6 @@ for obj in bpy.data.objects:
 
     createRenderComponent(componentsNode, modelFilepath)
     createCollisionComponents(componentsNode, colliderFile)
-
-
-
-    # rendererNode = ET.SubElement(componentsNode, "component")
-    # rendererNode.attrib["type"] = "Renderer"
-    # ET.SubElement(rendererNode, "textureIndex").text = "0"
-    # ET.SubElement(rendererNode, "meshOptimize").text = "true"
-    # ET.SubElement(rendererNode, "modelNormalization").text = "false"
-    # modelFileNamesNode = ET.SubElement(rendererNode, "modelFileNames")
-    # modelFileNameNode = ET.SubElement(modelFileNamesNode, "modelFileName")
-    # ET.SubElement(modelFileNameNode, "filename").text = modelFilepath
-    # ET.SubElement(modelFileNameNode, "lvlOfDetail").text = "0"
-
-
-
-    # obj.select_set(True)
-    # obj.location[0] = 0
-    # obj.location[1] = 0
-    # obj.location[2] = 0
-    # obj.rotation_euler[0] = 0
-    # obj.rotation_euler[1] = 0
-    # obj.rotation_euler[2] = 0
-    # obj.scale[0] = 1
-    # obj.scale[1] = 1
-    # obj.scale[2] = 1
-
-
-    # bpy.ops.wm.obj_export(export_selected_objects=True, filepath=dataPath + modelFilepath)
-
-    # obj.location = location
-    # obj.rotation_euler = rotation
-    # obj.scale = scale
-
-    # obj.select_set(False)
 
     id = id + 1
 
